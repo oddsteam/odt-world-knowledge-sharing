0c14e7ed7e1968759e01cfe79cd231edef07ab04
Thoritie <thohirh9@gmail.com>
2024-06-15 16:33:22 +0700

    Set turbo frame when add a new feedback :bear:

--- feedback/app/controllers/class_feedbacks_controller.rb
+++ feedback/app/controllers/class_feedbacks_controller.rb
@@ -1,17 +1,18 @@
 class ClassFeedbacksController < ApplicationController
 
   def new
-    @class_feedback = ClassFeedback.new 
+    @class_feedback = ClassFeedback.new
   end
 
   def create
-    puts params 
+    puts params
     puts params[:rating]
     puts params[:comment]
     class_feedback_params = params.require(:class_feedback).permit(:rating, :comment, :class_detail_id)
-    c = ClassFeedback.new(class_feedback_params)
+    @class_feedback = ClassFeedback.new(class_feedback_params)
     respond_to do |format|
-      if c.save
+      if @class_feedback.save
+        format.turbo_stream
         format.html do
           redirect_to home_index_path
         end
