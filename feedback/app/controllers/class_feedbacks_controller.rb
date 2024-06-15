63222572b6f8a395d11370a2e263ae2f5be7caaa
Under <un@odds.team>
2024-06-15 15:42:57 +0700

    edited create controller on class_feedbacks by Ai

--- feedback/app/controllers/class_feedbacks_controller.rb
+++ feedback/app/controllers/class_feedbacks_controller.rb
@@ -3,5 +3,10 @@ class ClassFeedbacksController < ApplicationController
   end
 
   def create
+    puts params 
+    puts params[:rating]
+    puts params[:comment]
+    c = ClassFeedback.new(rating: params[:rating], comment: params[:comment])
+    c.save
   end
 end
