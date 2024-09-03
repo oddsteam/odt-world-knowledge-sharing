299b10d063f11dafc0ecb4d8188343f722184f52
Chonlasith Jucksriporn <chonlasith@gmail.com>
2024-09-03 10:28:40 +0700

    chore: enable nix and flakes

--- flake.nix
+++ flake.nix
@@ -1,15 +1,30 @@
 {
   description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: 
+  let 
+    supportedSystems = [ "aarch64-darwin" ]; 
+  in 
+  {
+    devShells = builtins.foldl' (prev: system: prev // {
+      ${system}.default = nixpkgs.legacyPackages.${system}.mkShell {
+        buildInputs = with nixpkgs.legacyPackages.${system}; [
+          ruby_3_3
+          playwright
+          playwright-driver
+        ];
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+        shellHook = ''
+          export PLAYWRIGHT_BROWSERS_PATH=${nixpkgs.legacyPackages.${system}.playwright-driver.browsers}
+          export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+        '';
+      };
+    }) {} supportedSystems;
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    packages = builtins.foldl' (prev: system: prev // {
+      ${system}.default = nixpkgs.legacyPackages."${system}".hello; 
+    }) {} supportedSystems;
   };
 }
