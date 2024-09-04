dac27878daa8089b2fc9cb55b4830847afa562c2
Chonlasith Jucksriporn <chonlasith@gmail.com>
2024-09-04 22:10:09 +0700

    feat: Add mock login form

--- world/config/tailwind.config.js
+++ world/config/tailwind.config.js
@@ -5,15 +5,12 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-      aspectRatio: {
-        '4/3': '4 / 3',
       },
     },
   },
