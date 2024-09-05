aa8689c48287a87ff27024258c172a43e5141295
Chonlasith Jucksriporn <chonlasith@gmail.com>
2024-09-05 16:37:00 +0700

    chore: Move data migration to seed

--- README.md
+++ README.md
@@ -6,4 +6,12 @@
 
 If you are using nix compatible environment like Linux, macOS, Windows WSL2, you can try `nix` to control development environment. For more information, visit [Play Nix](https://github.com/chonla/play-nix).
 
+### Migrate World Data
+
+In development environment, I have provided some mock data which can be imported into development database.
+
+```bash
+./bin/rails db:seed
+```
+
 More detail about nix, visit [nixos.org](https://nixos.org/).
--- world/db/seeds.rb
+++ world/db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+Member.delete_all
+ActiveRecord::Base.connection.execute("DELETE FROM sqlite_sequence WHERE name='members';")
+
+members = [
+    ["วิมลรัตน์", "คำเอี่ยม", "มะเฟือง", "wimonrat", "wimonrat123"],
+    ["ชยพัทธ์", "แก้ววิไล", "วอลล์", "chayapat", "chayapat123"],
+    ["อนันตญา", "ไชยสงคราม", "ไข่มุก", "anantaya", "anantaya123"],
+    ["พริมา", "วัฒนโกศล", "ต้นอ้อ", "prima", "prima123"],
+    ["ชยุตรา", "ศิริธาดา", "จ้า", "chayutara", "chayutara123"],
+    ["ธัชพล", "สันติวงษ์", "มัค", "tatchapol", "tatchapol123"],
+    ["ถิรพล", "อนันต์ศัพทเจริญ", "เพิ่ม", "thirapol", "thirapol123"],
+    ["ธนา", "วิชิตรัตนาพร", "มัค", "thana", "thana123"],
+    ["ธยาดา", "อินทรประสิทธิ์", "แบมบี้", "thayada", "thayada123"],
+    ["รัญชนา", "วีรภัทรเมธี", "เก๋", "ranchana", "ranchana123"]
+]
+
+members.each do |member|
+    firstname, lastname, nickname, login, password = member
+    m = Member.new(
+        firstname: firstname,
+        lastname: lastname,
+        nickname: nickname,
+        login: login,
+        password: password
+    )
+    m.save
+end
