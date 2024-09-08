7f4a2a8b2a39749b197a99d1b9c4dedd05b35a05
Chonlasith Jucksriporn <chonlasith@gmail.com>
2024-09-08 21:48:10 +0700

    chore: Remove unused gem

--- world/config/initializers/google_sign_in.rb
+++
@@ -1,5 +0,0 @@
-# config/initializers/google_sign_in.rb
-Rails.application.configure do
-    config.google_sign_in.client_id     = ENV['google_sign_in_client_id']
-    config.google_sign_in.client_secret = ENV['google_sign_in_client_secret']
-end
--- world/config/routes.rb
+++ world/config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   get "login" => "session#login", as: :login
   get "logout" => "session#destroy", as: :logout
 
-  get "login/create", to: "logins#create", as: :create_login
-
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
