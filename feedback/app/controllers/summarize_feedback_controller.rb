4e12609eb95028ede35c3a00f589b680f6a8279f
fahlallaaaa <tadsika@odds.team>
2024-09-09 22:40:55 +0700

    calculate avg.trainer rating and avg.recommend by fah fah

--- feedback/app/controllers/summarize_feedback_controller.rb
+++ feedback/app/controllers/summarize_feedback_controller.rb
@@ -10,15 +10,33 @@ class SummarizeFeedbackController < ApplicationController
       @record_count = ClassFeedback
         .where(class_detail_id: params[:class_detail_id])
         .select(:class_detail_id)
-        .group(:class_detail_id)
         .count
       @courses_rating = ClassFeedback
       .select(:class_detail_id, 'ROUND( AVG(rating), 2 ) AS average_rating')
       .group(:class_detail_id)
       .having(class_detail_id: params[:class_detail_id])
       .first
+      
       @recommend = ClassFeedback
       .where(class_detail_id: params[:class_detail_id])
+      .group(:recommend)
+      .count[true]/@record_count.to_f*100
+
+      @trainer_rating = ClassFeedback
+      .select(:class_detail_id, 'ROUND( AVG(rating_trainer), 2 ) AS average_trainer_rating')
+      .first
+
+  
+
+
+
+  
+  
+
+
+  
+    
+      
 
     
     end
