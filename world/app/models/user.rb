32af6375c30c44e15e756cdfdf9f473f0a0839a6
Chonlasith Jucksriporn <chonlasith@gmail.com>
2024-09-10 09:16:03 +0700

    chore: Collect user profile from OAuth2

--- world/app/models/user.rb
+++ world/app/models/user.rb
@@ -11,6 +11,9 @@ class User < ApplicationRecord
   def self.create_from_provider_data(provider_data)
     where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do |user|
       user.email = provider_data.info.email
+      user.profile_picture = provider_data.info.image
+      user.firstname = provider_data.info.first_name
+      user.lastname = provider_data.info.last_name
       user.password = Devise.friendly_token[0, 20]
     end
   end
