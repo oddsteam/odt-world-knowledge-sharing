c37a17533e80cd04ac0943c0969448bbf315983c
Chonlasith Jucksriporn <chonlasith@gmail.com>
2024-09-12 17:05:16 +0700

    feat: Add mentors controller

---
+++ world/test/controllers/mentors_controller_test.rb
@@ -0,0 +1,8 @@
+require "test_helper"
+
+class MentorControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get mentor_index_url
+    assert_response :success
+  end
+end
