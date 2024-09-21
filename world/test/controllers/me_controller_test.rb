d389f4fb9d7473798399597f858f6d07ba360216
Chonlasith Jucksriporn <chonlasith@gmail.com>
2024-09-21 15:07:27 +0700

    chore: Add test

--- world/test/controllers/me_controller_test.rb
+++ world/test/controllers/me_controller_test.rb
@@ -1,8 +1,27 @@
 require "test_helper"
 
 class MeControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
   test "should be redirected to sign in page if unauthenticated" do
     get me_index_url
     assert_redirected_to "/users/sign_in"
+  end
+
+  test "should be able to authen" do
+    require "JSON"
+    provider_data = {
+      provider: "odt",
+      uid: 123,
+      info: {
+        email: "test@mailinator.com",
+        image: "",
+        first_name: "m",
+        last_name: "x"
+      }
+    }.to_json
+    sign_in User.create_from_provider_data(JSON.parse(provider_data, object_class: OpenStruct))
+    get me_index_url
+    assert_response :success
   end
 end
